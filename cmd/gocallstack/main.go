8a902891b97e49a5fc1aba46f8e88783379cdc10
chenxing <chenxing@bookan.com.cn>
2023-06-29 16:34:10 +0800

    functrace

--- cmd/gocallstack/main.go
+++ cmd/gocallstack/main.go
@@ -2,152 +2,55 @@
 package main
 
 import (
-	"debug/elf"
-	"log"
+	"fmt"
+	"github.com/go-delve/delve/pkg/proc"
+	"github.com/go-delve/delve/pkg/proc/native"
 	"os"
-	"os/exec"
 	"strings"
-	"syscall"
 )
-
-func getBk(symbols []elf.Symbol, funName string) uint64 {
-	for _, symbol := range symbols {
-		if symbol.Name == funName {
-			return symbol.Value
-		}
-	}
-	return 0
-}
-
-func getSymbol(symbols []elf.Symbol, ptr uint64) string {
-	for _, symbol := range symbols {
-		if symbol.Value <= ptr && ptr-symbol.Value <= symbol.Size {
-			return symbol.Name
-		}
-	}
-	return "unknow"
-}
-
-func setPC(pid int, pc uint64) {
-	var regs syscall.PtraceRegs
-	err := syscall.PtraceGetRegs(pid, &regs)
-	if err != nil {
-		log.Fatal(err)
-	}
-	regs.SetPC(pc)
-	err = syscall.PtraceSetRegs(pid, &regs)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func cont(pid int) {
-	err := syscall.PtraceCont(pid, 0)
-	if err != nil {
-		log.Fatal("cont ", err, pid)
-	}
-}
-
-func setBreakpoint(pid int, breakpoint uint64) byte {
-	original := make([]byte, 1)
-	_, err := syscall.PtracePeekData(pid, uintptr(breakpoint), original)
-	if err != nil {
-		log.Fatal("setBreakpoint ", err)
-	}
-	_, err = syscall.PtracePokeData(pid, uintptr(breakpoint), []byte{0xCC})
-	if err != nil {
-		log.Fatal("setBreakpoint ", err)
-	}
-	return original[0]
-}
-
-func clearBreakpoint(pid int, breakpoint uint64, ori byte) {
-	original := make([]byte, 1)
-	original[0] = ori
-	_, err := syscall.PtracePokeData(pid, uintptr(breakpoint), original)
-	if err != nil {
-		log.Fatal("clearBreakpoint ", err)
-	}
-}
-
-func getCurrentRegs(pid int) syscall.PtraceRegs {
-	var regs syscall.PtraceRegs
-	err := syscall.PtraceGetRegs(pid, &regs)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return regs
-}
 
 func main() {
 	if len(os.Args) != 2 {
 		println("usage: gocallstack /path/to/exe")
 		return
 	}
-	cmd := exec.Command(os.Args[1])
-	cmd.Args = os.Args[1:]
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.SysProcAttr = &syscall.SysProcAttr{Ptrace: true}
-	err := cmd.Start()
+	targetGroup, err := native.Launch(os.Args[1:], "", 0, []string{}, "", [3]string{})
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	err = cmd.Wait()
-	pid := cmd.Process.Pid
-	log.Printf("pid:%d %v\n", pid, err)
+	targetList := targetGroup.Targets()
+	for _, target := range targetList {
+		for bid, fn := range target.Process.BinInfo().Functions {
+			if fn.Entry == 0 || strings.HasPrefix(fn.Name, "runtime.") {
+				continue
+			}
 
-	// 打开go二进制文件
-	file, err := elf.Open(os.Args[1])
-	if err != nil {
-		log.Println("Error opening file")
-	}
-	defer file.Close()
-
-	// 获取符号表
-	symbols, err := file.Symbols()
-	if err != nil {
-		log.Println("Error getting symbols")
+			_, err = target.SetBreakpoint(bid, fn.Entry, proc.UserBreakpoint, nil)
+			if err != nil {
+				fmt.Println(fn.Name, err.Error())
+			}
+		}
 	}
 
-	// 设置所有的断点
-	breakpointMap := make(map[uint64]byte)
-	for _, symbol := range symbols {
-		if symbol.Size == 0 {
-			continue
+	err = targetGroup.Continue()
+	for err == nil {
+		for _, target := range targetList {
+			gid := int64(0)
+			tid := target.CurrentThread().ThreadID()
+			bp := target.CurrentThread().Breakpoint().Breakpoint
+			if bp == nil {
+				continue
+			}
+			if target.SelectedGoroutine() != nil {
+				gid = target.SelectedGoroutine().ID
+			}
+			fmt.Printf("%8d %8d %s-%x\n", tid, gid, bp.FunctionName, bp.Addr)
 		}
-		if strings.Contains(symbol.Name, "runtime.") {
-			continue
+		err = targetGroup.Next()
+		if err != nil && err.Error() == "next while nexting" {
+			// 线程正在nexting的时候被中断，如果继续Next()会报错，所以用Continue()
+			err = targetGroup.Continue()
 		}
-		if strings.Contains(symbol.Name, "internal/") {
-			continue
-		}
-		if strings.Contains(symbol.Name, "_cgo_") {
-			continue
-		}
-		if !strings.Contains(symbol.Name, "_rt0_") {
-			continue
-		}
-		original := setBreakpoint(pid, symbol.Value)
-		breakpointMap[symbol.Value] = original
 	}
-
-	var ws syscall.WaitStatus
-	cont(pid)
-	waitPid, waitErr := syscall.Wait4(pid, &ws, syscall.WALL, nil)
-	for waitErr == nil {
-		if ws == 0x057f {
-			regs := getCurrentRegs(waitPid)
-			name := getSymbol(symbols, regs.Rip)
-			log.Printf("pid:%d %x %s %x\n", waitPid, ws, name, regs.Rip)
-
-			breakpoint := regs.Rip - 1
-			clearBreakpoint(waitPid, breakpoint, breakpointMap[breakpoint])
-			setPC(waitPid, breakpoint)
-		}
-
-		cont(waitPid)
-		waitPid, waitErr = syscall.Wait4(pid, &ws, syscall.WALL, nil)
-	}
-	log.Printf("%s %x \n", waitErr.Error(), ws)
+	fmt.Println(err.Error())
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/xing393939/gotools
 go 1.16
 
 require (
+	github.com/go-delve/delve v1.21.0 // indirect
 	github.com/goccy/go-graphviz v0.0.9
 	github.com/spf13/afero v1.8.2
-	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
 	golang.org/x/tools v0.1.12
 )
